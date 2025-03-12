1af1a066bf34cce57a6c111a42b6d0147adb0a11
rebisha1403 <sivarebisha@gmail.com>
2025-03-12 15:13:58 +0530

    fix:Sivarebisha

---
+++ concatenation.java
@@ -0,0 +1,7 @@
+public class concatenation{
+    public static void main(String[] args){
+        String firstname="Siva";
+        String lastname="Rebisha";
+        System.out.println(firstname.concat(lastname));
+    }
+}
---
+++ variable.java
@@ -0,0 +1,6 @@
+public class variable{
+    public static void main(String[] args){
+    String name="Siva rebisha";
+    System.out.println(name); 
+}
+}
