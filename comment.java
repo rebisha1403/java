f7a7ca33303ccbd0796aae01b14aef645254ceb6
rebisha1403 <sivarebisha@gmail.com>
2025-03-12 14:38:13 +0530

    fix:My name is rbsha

---
+++ comment.java
@@ -0,0 +1,6 @@
+public class comment{
+    public static void main(String[] args){
+        System.out.println("My name is rebisha");//used to print the statement
+        System.out.println("My favourite color is pinkkkuuh");//printing statement used in java
+    }
+}
