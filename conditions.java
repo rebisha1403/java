e140e83fca44f2033aec04f35d2c9c0cee29cdcc
rebisha1403 <sivarebisha@gmail.com>
2025-03-13 15:09:33 +0530

    iff

---
+++ conditions.java
@@ -0,0 +1,16 @@
+public class conditions{
+    public static void main(String[] args){
+        int age=19;
+        System.out.println("Age="+age);
+        if(age>18)
+        {
+            System.out.println("You are eligible to vote");
+
+        }
+    else
+    {
+        System.out.println("You are not eligible to vote");
+   }
+    }
+
+}
