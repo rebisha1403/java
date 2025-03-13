72c3d759e7a5ea3596dc5b334ecaca0e111f778e
rebisha1403 <sivarebisha@gmail.com>
2025-03-13 10:25:13 +0530

    fix:My name is:Rebisha

---
+++ details.java
@@ -0,0 +1,13 @@
+public class details{
+    
+    public static void main(String[]args){
+        String name="rebisha";
+        //String class="cse";
+        int Mark=88;
+        char Grade='A';
+        System.out.println("My name is:"+name);
+        //System.out.println("class:"+class);
+        System.out.println("Mark:"+Mark);
+        System.out.println("Grade:"+Grade);
+    }
+}
