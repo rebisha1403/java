8e431e91fd2629cd4c083c5dc3053684c8e00e9b
rebisha1403 <sivarebisha@gmail.com>
2025-03-13 11:23:51 +0530

    res

---
+++ operators.java
@@ -0,0 +1,13 @@
+public class operators{
+    public static void main(String[] args){
+        int a=14,b=3;
+        int c=a+b;
+        int d=a-b;
+        int e=a*b;
+        int f=a%b;
+        System.out.println("addition:"+c);
+        System.out.println("subtraction:"+d);
+        System.out.println("multiplication:"+e);
+        System.out.println("division:"+f);
+    }
+}
