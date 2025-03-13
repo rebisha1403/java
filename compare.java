8db13f117ffbf7f6068921736da3dfcd846559a0
rebisha1403 <sivarebisha@gmail.com>
2025-03-13 11:57:36 +0530

    fix:answer

---
+++ compare.java
@@ -0,0 +1,10 @@
+public class compare{
+    public static void main(String[] args){
+        String x="rebisha";
+        String y="Rebisha";
+        if(x==y)
+        System.out.println("True");
+        else
+        System.out.println("False");
+    }
+}
