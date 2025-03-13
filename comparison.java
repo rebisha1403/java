c64072040620d0909945ba2d24ee84da2445da9d
rebisha1403 <sivarebisha@gmail.com>
2025-03-13 11:59:32 +0530

    ans

---
+++ comparison.java
@@ -0,0 +1,9 @@
+public class comparison{
+    public static void main(String[] args){
+        int x=14,y=24;
+        if(x<y)
+        System.out.println("True");
+        else
+        System.out.println("False");
+    } 
+}
