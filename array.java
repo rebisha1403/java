c26039a32bb830f26f7238e3a50d2c8f7157fc8d
rebisha1403 <sivarebisha@gmail.com>
2025-03-14 13:48:07 +0530

    array

---
+++ array.java
@@ -0,0 +1,8 @@
+public class array
+{
+    public static void main(String[] args)
+    {
+        String froots[]={"apple","orange","watermelon","papaya","grapes","pineapple"};
+        System.out.println(froots[5]);
+    }
+}
