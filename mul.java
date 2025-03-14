c2c5fcfa2437d906ace3f5684ec2eb7527fa52b1
rebisha1403 <sivarebisha@gmail.com>
2025-03-14 13:25:46 +0530

    multyply

---
+++ mul.java
@@ -0,0 +1,12 @@
+public class mul{
+    static void multiply()
+    {
+        int x=19,y=12;
+        int z=x*y;
+        
+        System.out.println("the result"+z);
+    }
+    public static void main(String[] args){
+    multiply();
+    }
+}
