5a7740c416c3a4dfdecdf1376933708d64a85f7f
rebisha1403 <sivarebisha@gmail.com>
2025-03-14 13:25:00 +0530

    rebisha

---
+++ rbsha.java
@@ -0,0 +1,9 @@
+public class rbsha{
+    static void rbshaa()
+    {
+        System.out.println("Happy Birthday rebisha");
+    }
+    public static void main(String[] args){
+        rbshaa();
+    }
+}
