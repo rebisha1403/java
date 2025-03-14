1d4f20b3201097f4db735d272160b410fbf851f0
rebisha1403 <sivarebisha@gmail.com>
2025-03-14 14:45:10 +0530

    rename

---
+++ rename.java
@@ -0,0 +1,9 @@
+public class rename
+{
+    public static void main(String[] args)
+    {
+        String fruits[]={"apple","mango"};
+        fruits[0]="pineapple";
+        System.out.println(fruits[0]);
+    }
+}
