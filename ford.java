1019e8f4c26c3404f1f04f22ef06115576fd11d5
rebisha1403 <sivarebisha@gmail.com>
2025-03-14 11:49:48 +0530

    fordd

---
+++ ford.java
@@ -0,0 +1,11 @@
+public class ford
+{
+    public static void main(String[] args)
+    {  int i=20;
+        
+        for(i=20;i>=10;i--)
+        {
+        System.out.println(i);
+        }
+    }
+}
