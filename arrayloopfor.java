138145681c353c93c0b20ffb86956a198d12a643
rebisha1403 <sivarebisha@gmail.com>
2025-03-14 15:40:32 +0530

    array

---
+++ arrayloopfor.java
@@ -0,0 +1,9 @@
+public class arrayloopfor{
+    public static void main(String[] args){
+        String froots[]={"apple","pineapple","watermelon","mango","jackfruit"};
+        for(int i=0;i<=5;i++)
+        {
+            System.out.println(froots[i]);
+        }
+    }
+}
