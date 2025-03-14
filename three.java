593b00a0e362676c85d57656041d5ffe9c65d757
rebisha1403 <sivarebisha@gmail.com>
2025-03-14 14:28:17 +0530

    three

---
+++ three.java
@@ -0,0 +1,8 @@
+public class three
+{
+    public static void main(String[] args)
+    {
+        int [][]numbers ={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        System.out.println(numbers[2][3]);
+    }
+}
