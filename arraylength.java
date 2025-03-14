b6ce56b0684d7f7d37c8d5e7c61e407e9612c967
rebisha1403 <sivarebisha@gmail.com>
2025-03-14 13:55:24 +0530

    arraylen

---
+++ arraylength.java
@@ -0,0 +1,8 @@
+public class arraylength
+{
+    public static void main(String[] args)
+    {
+        String froots[]={"apple","orange","watermelon","papaya","grapes","pineapple"};
+        System.out.println(froots.length);
+    }
+}
