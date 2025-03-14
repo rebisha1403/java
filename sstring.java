71062f57888b59ebe2d5897dd883132a19f1e9c0
rebisha1403 <sivarebisha@gmail.com>
2025-03-14 10:56:53 +0530

    fruity

---
+++ sstring.java
@@ -0,0 +1,13 @@
+public class sstring
+{
+    public static void main(String[] args)
+    {
+        int i=1;
+        String fruitee="watermelon";
+        do{
+            System.out.println(fruitee);
+            i++;
+        }
+        while(i<=5);
+    }
+}
