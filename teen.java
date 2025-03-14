7bb32a627eff5e3611473405ae6ed2137bb51b2d
rebisha1403 <sivarebisha@gmail.com>
2025-03-14 10:43:40 +0530

    ten

---
+++ teen.java
@@ -0,0 +1,14 @@
+public  class teen
+{
+    public static void main(String[] args)
+{
+        int i=1;
+        System.out.println("The numbers are:");
+        
+        while(i<=5)
+        {   
+           System.out.println(10);
+            i++;
+        }
+    }
+}
