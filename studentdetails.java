b53cae24a911e10f4f96bdb0dda15c46a7a3b69f
rebisha1403 <sivarebisha@gmail.com>
2025-03-15 09:59:48 +0530

    student

---
+++ studentdetails.java
@@ -0,0 +1,14 @@
+class Student {
+    int id;
+    String name;
+}
+
+public class studentdetails {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.id = 105;
+        s1.name = "Rebisha";
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+    }
+}
