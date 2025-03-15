2852f119c7afca0aeea0993fa237e37f5759dc21
rebisha1403 <sivarebisha@gmail.com>
2025-03-15 12:01:56 +0530

    scanning

---
+++ scan.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class scan{
+    public static void main(String[] args)
+    {
+        Scanner s1=new Scanner(System.in);
+        System.out.println("Enter username");
+        String username=s1.nextLine();
+        System.out.println("The username is"+username);
+    }
+}
