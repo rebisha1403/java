b2dc1195eadacf9365fcefe98fafcd003861bf16
rebisha1403 <sivarebisha@gmail.com>
2025-03-15 10:27:21 +0530

    BILL

---
+++ billingsystem.java
@@ -0,0 +1,13 @@
+public class billingsystem{
+    public static void main(String[] args){
+        int invoiceno[]={3425,6789,2234,5678,6654};
+        {
+           System.out.println("The invoice numbers are:");
+            for(int i=0;i<=5;i++)
+            {
+                System.out.println(invoiceno[i]);
+
+            }
+        }
+    }
+}
