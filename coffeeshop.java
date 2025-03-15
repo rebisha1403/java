520b4038973e6625c96c5e77c0b145866b3de759
rebisha1403 <sivarebisha@gmail.com>
2025-03-15 11:34:17 +0530

    rebisha coffeeshop

---
+++ coffeeshop.java
@@ -0,0 +1,27 @@
+public class coffeeshop{
+    public static void main(String[] args){
+        int menunumber=5;
+        switch(menunumber)
+        {
+            case 1:
+                System.out.println(" Hot coffee ");
+                break;
+            case 2:
+                System.out.println("Cold coffee 2");
+                break;
+            case 3:
+                System.out.println("Green tea");
+                break;
+            case 4:
+                System.out.println("Mint tea");
+                break;
+            case 5:
+                System.out.println("Ginger tea");
+                break;
+            default:
+                System.out.println("coffee and tea");
+                break;
+
+        }
+    }
+}
